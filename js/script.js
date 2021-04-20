0b1b6e8812418bdf949a78d011ddfcfae14f534a
xerces <67798774+xerces@users.noreply.github.com>
2021-04-20 18:16:43 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Yash", "John", "jatin", "Shashank", "Aditi", "James", "Harsh", "Jordan"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
